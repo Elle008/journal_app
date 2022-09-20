701b81a0cadcb99b8a8ba3019ab283dcdc766a24
Elle08 <maellaineanis@gmail.com>
2022-09-20 20:04:55 +0800

    move header sidebar and alerts to application template, generate controller and model for tasks

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,22 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_09_20_024338) do
+ActiveRecord::Schema[7.0].define(version: 2022_09_20_120416) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "categories", force: :cascade do |t|
     t.string "category_name"
     t.text "category_description"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "tasks", force: :cascade do |t|
+    t.text "description"
+    t.date "date"
+    t.time "time"
+    t.boolean "status_completed"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
